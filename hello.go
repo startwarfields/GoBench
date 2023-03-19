e7af0a9b3d21409075affc9cfce3a0ea0e4477f7
startwarfields <j.warfieldmeyer@gmail.com>
2023-03-19 12:30:29 -0700

    if you read this, hi

--- hello.go
+++ hello.go
@@ -12,8 +12,7 @@ var p1 = fmt.Println
 
 func main() {
   // Example code from tutorial https://www.youtube.com/watch?v=YzLrWHZa-Kc
- Convert() 
-}
+  }
 func Convert() {
   cv3:= "5000000"
   cv4, err := strconv.Atoi(cv3)
@@ -30,5 +29,15 @@ func Greet() {
     p1("Hello", name)
   } else {
     log.Fatal(err)
+  }
+}
+func AgeBirthday() {
+  iAge := 8
+  if (iAge >=1) && (iAge <18) {
+    p1("Important Birthday")
+  } else if (iAge == 21) || (iAge == 50) {
+    p1("Important Birthday")
+  }  else {
+    p1("Not an important birthday")
   }
 }
